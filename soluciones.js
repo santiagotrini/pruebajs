996736490cd09249a548cbde92a4ae2e588fc62a
Santiago Trini <santiagotrini@gmail.com>
2023-07-05 16:49:32 -0300

    primer commit

---
+++ soluciones.js
@@ -0,0 +1,12 @@
+function handleSubmit() {
+  // la pregunta 2
+}
+function circle(x,y,r) {
+  // la pregunta 3
+}
+function easterEgg() {
+  // la pregunta 4
+}
+function easterEasterEgg() {
+  // la pregunta 5
+}
